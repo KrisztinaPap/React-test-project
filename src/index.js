ee3f17087e77005e39ae4479bd61540a50b709a7
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-10 11:03:15 -0600

    Created Hello.js component;

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Hello from './components/Hello';
 
 ReactDOM.render(
   <React.StrictMode>
-    Hello World!!!
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
